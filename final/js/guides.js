bf3db0e4a2cfb7766f0ef69c81bfd08942766874
Kendra Anderson <kor04003@byui.edu>
2020-03-30 14:49:44 -0700

    reservations

--- final/js/guides.js
+++ final/js/guides.js
@@ -10,6 +10,8 @@ fetch(requestURL)
 
    for (let x = 0; x < guides.length; x++ ) {
      let section = document.createElement('section');
+     let info = document.createElement('div');
+     let pic = document.createElement('div');
      let name = document.createElement('h2');
      let age = document.createElement('p');
      let cert = document.createElement('p');
@@ -18,23 +20,26 @@ fetch(requestURL)
      let bio = document.createElement('p');
      let image = document.createElement('img');
 
+     info.className = "guide-info";
+     pic.classname = "guide-pic";
      name.textContent = guides[x].name;
      age.textContent = 'Age: ' + guides[x].age;
      cert.textContent = 'Certifications: ' + guides[x].certification;
      xp.textContent = 'Years Experience: ' + guides[x].experience;
      email.textContent = guides[x].email;
      bio.textContent = guides[x].biography;
-
      image.setAttribute('src', guides[x].photo);
      image.setAttribute('alt', guides[x].name);
 
-     section.appendChild(name);
-     section.appendChild(age);
-     section.appendChild(cert);
-     section.appendChild(xp);
-     section.appendChild(email);
-     section.appendChild(bio);
-     section.appendChild(image);
+     info.appendChild(name);
+     info.appendChild(age);
+     info.appendChild(cert);
+     info.appendChild(xp);
+     info.appendChild(email);
+     info.appendChild(bio);
+     pic.appendChild(image);
+     section.appendChild(info);
+     section.appendChild(pic);
       
      document.querySelector('div.guides').appendChild(section);
     }
